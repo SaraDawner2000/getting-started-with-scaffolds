51ee33701520e11db198ed2c6dc6de8cf3f26995
Natalie Demyanenko <83845798+SaraDawner2000@users.noreply.github.com>
2024-06-24 11:59:23 -0500

    finish scaffolding exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,27 @@
 Rails.application.routes.draw do
+  resources :books
+  # Routes for the Movie resource:
+
+  # CREATE
+  post("/movies", { controller: "movies", action: "create" })
+
+  get("/movies/new", { controller: "movies", action: "new" })
+
+  # READ
+  get("/movies", { controller: "movies", action: "index" })
+
+  get("/movies/:id", { controller: "movies", action: "show" })
+
+  # UPDATE
+
+  patch("/movies/:id", { controller: "movies", action: "update" })
+  get("/movies/:id/edit", { controller: "movies", action: "edit" })
+
+  # DELETE
+  delete("/movies/:id", { controller: "movies", action: "destroy" })
+
+  #------------------------------
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
